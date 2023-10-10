01ffa4d1fb9eddea02552ecf415772edb8ad5281
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-10-10 14:03:30 +0500

    Add files via upload

---
+++ trpo6/Classes/Cow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using trpo6.Windows;
+
+namespace trpo6
+{
+   public class Cow
+    {
+        public string Name { get; set; }
+        public string Voise { get; set; }
+        public string Image { get; set; }
+        public int Age { get; set; }
+
+        public string FullAgeString
+        {
+            get
+            {
+                return $"{DateTime.Now.ToShortDateString()} {Name} {Age} years";
+            }
+        }
+    }
+}
---
+++ trpo6/Classes/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpo6.Classes
+{
+   public class Product
+    {
+        public string ProductName { get; set; }
+        public string ImageFile { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+        public double Discount { get; set; }
+
+        public bool HasDiscount
+        {
+            get
+            {
+                return Discount > 0;
+            }
+        }
+
+        public decimal DiscountPrise
+        {
+            get
+            {
+                return Price * (decimal)(1 - Discount);
+            }
+        }
+    }
+}
